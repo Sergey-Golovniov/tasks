f5aff77f050b3039a11fa46cd93ce31d39a40bb2
SergeyGolovniov <sergei.golovniov@gmail.com>
2024-07-13 11:35:51 +0300

    js#5 solution

--- script.js
+++ script.js
@@ -3,7 +3,16 @@
 const messageQueue = [];
 
 let sendMessage = (message) => {
+  messageQueue.push(message);
+  console.log(`${message} has been added to the batch`)
 
+  if (messageQueue.length === 1) {
+    queueMicrotask(() => {
+      const json = JSON.stringify(messageQueue);
+      messageQueue.length = 0;
+      console.log(`messages batch: ${json}`);
+    });
+  }
 };
 
 sendMessage("first"); // first has been added to the batch
